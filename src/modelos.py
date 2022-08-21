8e3b89bab939ec59d03ad6a16f23288ce0ed3a6e
Murilo Sobreiro <murilosobreiro@hotmail.com>
2022-08-21 20:38:34 -0300

    Change: Teclado de dados

--- src/modelos.py
+++ src/modelos.py
@@ -4,6 +4,7 @@ from sqlmodel import SQLModel, Field
 
 class Termooo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    idJogador: int
     jogador: str
     jogo: str
     numero: str
