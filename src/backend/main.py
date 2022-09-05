5b9a89467482d730bf4d969a24d99e2458cadf5c
Murilo Sobreiro <murilosobreiro@hotmail.com>
2022-09-05 11:03:29 -0300

    Add: CORSMiddleWare

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import src.backend.utils as utils
 
-app = FastAPI()
-
+app = FastAPI(title="RPG API", version="0.6.9")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
